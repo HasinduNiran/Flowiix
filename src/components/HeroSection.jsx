8cf16e77fe4e98491a78e894804a34bfff92c186
iamsahan <weerakkodyse@gmail.com>
2025-03-30 20:03:21 +0530

    fixed overflow issue

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
-    <section className="grid grid-cols-2 px-10 h-screen">
+    <section className="grid grid-cols-2 px-10 h-screen max-w-screen overflow-x-hidden">
       <div className="ml-24 mt-40">
         <h2 className="text-white text-5xl">Find the project you love</h2>
         <p className="text-white mt-5">
@@ -16,20 +16,19 @@ const HeroSection = () => {
           Explore
         </button>
       </div>
-      <div>
-        <motion.img
-          className="absolute top-1/2 translate-x-96 opacity-80 lg:-translate-y-3/5 "
-          style={{
-            width: "60vw",
-            maxWidth: "1100px",
-            height: "auto",
-          }}
-          src="/asset/images/flat-ring.png"
-          alt="Rotating"
-          animate={{ rotate: 360 }}
-          transition={{ duration: 100, repeat: Infinity, ease: "linear" }}
-        />
-      </div>
+
+      <motion.img
+        className="translate-x-1/3 opacity-80 lg:-translate-y-1/6 overflow-hidden"
+        style={{
+          width: "70vw",
+          maxWidth: "1100px",
+          height: "auto",
+        }}
+        src="/asset/images/flat-ring.png"
+        alt="Rotating"
+        animate={{ rotate: 360 }}
+        transition={{ duration: 100, repeat: Infinity, ease: "linear" }}
+      />
     </section>
   );
 };
