5c597432fb910c19a9a714a8afd443d0695f0489
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2025-03-30 14:50:09 +0530

    add ServiceStats component and banner image to About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,12 @@
+import ServiceStats from "@/src/components/serviceStats";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <ServiceStats />
+    </div>
+  );
 };
 
 export default page;
---
+++ src/components/serviceStats.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+
+export default function ServiceStats() {
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center p-6">
+      <div className="w-full max-w-6xl">
+        <div className="flex flex-col items-center justify-center ">
+          <div className="bg-white w-[1048px] h-[225px] rounded-xl shadow-lg p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 z-20">
+            {[...Array(4)].map((_, index) => (
+              <div key={index} className="text-center">
+                <h3 className="text-lg font-semibold">Title 1</h3>
+                <p className="text-gray-500 text-sm">
+                  Lorem ipsum dolor sit amet, consectetur
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="relative -top-40 z-0">
+          <Image
+            src="/asset/images/banner.jpeg"
+            alt="Background"
+            width={1312}
+            height={395}
+            className="w-[1312px] h-[395px] rounded-lg object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
